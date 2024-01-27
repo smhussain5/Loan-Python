158fdefbc5d4b905efed8c1df42ebdaf93ef1468
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2024-01-27 13:46:39 -0600

    Reformatted code with Python Black and Pylint (Score: 10/10)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from pyfiglet import Figlet
+"""
+Terminal application that calculates payment amount per payment period
+"""
+
 import locale
+from pyfiglet import Figlet
+
 locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
 # WELCOME SCREEN
@@ -11,9 +16,9 @@ print("WELCOME TO LOAN CALCULATOR.")
 print("It's simple. Just follow the prompts!\n")
 
 # VARIABLES
-play_again = True
+PLAY_AGAIN = True
 
-while play_again:
+while PLAY_AGAIN:
     loan_amount = float(input("\nHow much did you borrow?: "))
     interest_rate = float(input("What is your annual interest rate (%)?: "))
     payments_per_year = int(input("How many payments per year?: "))
@@ -24,15 +29,14 @@ while play_again:
 
     # MESSAGE
 
-    print(f"\nSo you borrowed {locale.currency(loan_amount)} with an annual interest rate of {interest_rate}%?")
+    print(
+        f"\nYou borrowed {locale.currency(loan_amount)} with an interest rate of {interest_rate}%?"
+    )
     print(f"With {payments_per_year} payments per year, each payment should be...\n")
     print(f"{locale.currency(payment_amount)}\n")
 
     replay_choice = input("Try another calculation? Enter Y/N...: ")  # APP REPLAY
 
-    if replay_choice.upper() == "Y":
-        play_again = True
-    else:
-        play_again = False
+    PLAY_AGAIN = bool(replay_choice.upper() == "Y")
 
 print("Thanks for playing! See you again next time!")  # APP END
